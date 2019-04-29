d7c97122792df86e98271482fd37878c6d2f5447
emilio barrera <barrera_emilio@hotmail.com>
2019-04-29 18:59:55 +0100

    Add project files.

---
+++ WebAPIAngular7CRUD/Models/PaymentDetailsConetx.cs
@@ -0,0 +1,18 @@
+﻿namespace WebAPIAngular7CRUD.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    public class PaymentDetailsConetx : DbContext
+    {
+        public PaymentDetailsConetx(DbContextOptions<PaymentDetailsConetx> options):base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+
+    }
+}
